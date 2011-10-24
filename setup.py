b94432f67d89e08173c53288f8ef2bda1635a1a8
Daniel Gray <dan@technigami.com>
2011-10-24 11:29:48 -0700

    adding explanation

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ setup(
     scripts = ['twango/twango-admin.py'],
 	package_data = {
 	        # If any package contains *.txt or *.rst files, include them:
-	        '': ['*.sh','*.txt', '*.html','*.js','*.css','*.png','*.jpg','*.pdf'],
-			'twango/template/default/src/media':['*'],
-			'twango/template/default/src/settings':['*'],
-			'twango/template/default/src/templates':['*'],
+			'twango.template.default.src':['template/default/src/media/*'],
+			'twango.template.default.src':['template/default/src/settings/*'],
+			'twango.template.default.src':['template/default/src/templates/*'],
 	    },
     author = "Daniel Gray",
     author_email = "dan@technigami.com",
