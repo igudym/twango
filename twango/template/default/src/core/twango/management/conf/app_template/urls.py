80ba9e4fdd50634504a4968372465f51bcbdcb66
Daniel Gray <dan@technigami.com>
2011-10-24 22:35:33 -0700

    Adding some basic templates to the twango project to create the beginings of a more dynamic project creation workflow

--- twango/template/default/src/core/twango/management/conf/app_template/urls.py
+++ twango/template/default/src/core/twango/management/conf/app_template/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
+# You may have to replace 'views' with 'apps.(package).views below.'
+# this is a known issue
+
 urlpatterns = patterns('views',
         (r'^$','index'),
 )
--- twango/template/default/src/core/twango/management/conf/app_template/views.py
+++ twango/template/default/src/core/twango/management/conf/app_template/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-        return render_to_response('index.html')
+    """
+    you will have to replace "index.html"
+    with (yourappname)/index.html
+    and createa folder named (yourappname) in the templates directory.
+    """    
+
+    return render_to_response('index.html')
